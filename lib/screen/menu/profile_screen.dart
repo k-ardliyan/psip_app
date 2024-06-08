3ca427257dbd91ff4ca9f4770dc9ab5fc53890a2
k.ardliyan <k.ardliyan@gmail.com>
2024-06-08 03:07:54 +0000

    fix: navigasi antar halaman menggunakan getx - init idx folder - perapian pubspec.yaml - inisialisasi routing navigasi get - fix bug user null di home screen sederhana

--- lib/screen/menu/profile_screen.dart
+++ lib/screen/menu/profile_screen.dart
@@ -5,7 +5,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:psip_app/model/user_model.dart';
 
@@ -42,175 +41,177 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Stack(
-            clipBehavior: Clip.none,
-            alignment: Alignment.center,
-            children: [
-              buildCoverImage(),
-              Positioned(
-                top: coverHeight / 1.75,
-                child: Card(
-                  color: Colors.white,
-                  child: SizedBox(
-                    height: coverHeight / 1.25,
-                    width: MediaQuery.of(context).size.width - 25,
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(20),
-                              child: CircleAvatar(
-                                radius: 40,
-                                backgroundColor: Colors.primaries[
-                                    Random().nextInt(Colors.primaries.length)],
-                                child: Text(
-                                  user?.displayName
-                                          ?.substring(0, 2)
-                                          .toUpperCase() ??
-                                      '',
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                    fontSize: 25,
-                                    letterSpacing: 1,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              clipBehavior: Clip.none,
+              alignment: Alignment.center,
+              children: [
+                buildCoverImage(),
+                Positioned(
+                  top: coverHeight / 1.75,
+                  child: Card(
+                    color: Colors.white,
+                    child: SizedBox(
+                      height: coverHeight / 1.25,
+                      width: MediaQuery.of(context).size.width - 25,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(20),
+                                child: CircleAvatar(
+                                  radius: 40,
+                                  backgroundColor: Colors.primaries[Random()
+                                      .nextInt(Colors.primaries.length)],
+                                  child: Text(
+                                    user?.displayName
+                                            ?.substring(0, 2)
+                                            .toUpperCase() ??
+                                        '',
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      fontSize: 25,
+                                      letterSpacing: 1,
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            Text.rich(
-                              TextSpan(
-                                children: [
-                                  TextSpan(
-                                    text:
-                                        "${user?.displayName?.toUpperCase() ?? '*****'}\n",
-                                    style: GoogleFonts.poppins(
-                                      textStyle: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20,
+                              Text.rich(
+                                TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text:
+                                          "${user?.displayName?.toUpperCase() ?? '*****'}\n",
+                                      style: GoogleFonts.poppins(
+                                        textStyle: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20,
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                  TextSpan(
-                                    text: FirebaseAuth
-                                            .instance.currentUser?.uid ??
-                                        '*****',
-                                  ),
-                                ],
+                                    TextSpan(
+                                      text: FirebaseAuth
+                                              .instance.currentUser?.uid ??
+                                          '*****',
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: OutlinedButton.icon(
-                            onPressed: () {},
-                            style: OutlinedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: OutlinedButton.icon(
+                              onPressed: () {},
+                              style: OutlinedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                fixedSize: Size(
+                                  MediaQuery.of(context).size.width,
+                                  45,
+                                ),
+                                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                                iconColor: Colors.black87,
                               ),
-                              fixedSize: Size(
-                                MediaQuery.of(context).size.width,
-                                45,
-                              ),
-                              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                              iconColor: Colors.black87,
-                            ),
-                            icon: const Icon(FluentIcons.person_20_filled),
-                            label: Text(
-                              "Lihat Profil",
-                              style: GoogleFonts.poppins(
-                                textStyle:
-                                    const TextStyle(color: Colors.black87),
+                              icon: const Icon(FluentIcons.person_20_filled),
+                              label: Text(
+                                "Lihat Profil",
+                                style: GoogleFonts.poppins(
+                                  textStyle:
+                                      const TextStyle(color: Colors.black87),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: coverHeight / 2,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              fixedSize: Size(MediaQuery.of(context).size.width - 25, 45),
+              ],
             ),
-            child: ListTile(
-              dense: true,
-              horizontalTitleGap: 10,
-              leading: const Icon(FluentIcons.password_reset_48_regular),
-              title: Text(
-                "Ganti Kata Sandi",
-                style: GoogleFonts.poppins(
-                  textStyle: const TextStyle(
-                    fontSize: 17,
+            SizedBox(
+              height: coverHeight / 2,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                fixedSize: Size(MediaQuery.of(context).size.width - 25, 45),
+              ),
+              child: ListTile(
+                dense: true,
+                horizontalTitleGap: 10,
+                leading: const Icon(FluentIcons.password_reset_48_regular),
+                title: Text(
+                  "Ganti Kata Sandi",
+                  style: GoogleFonts.poppins(
+                    textStyle: const TextStyle(
+                      fontSize: 17,
+                    ),
                   ),
                 ),
+                trailing: const Icon(FluentIcons.arrow_right_48_regular),
               ),
-              trailing: const Icon(FluentIcons.arrow_right_48_regular),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              fixedSize: Size(MediaQuery.of(context).size.width - 25, 45),
+            const SizedBox(
+              height: 20,
             ),
-            child: ListTile(
-              dense: true,
-              horizontalTitleGap: 10,
-              leading: const Icon(
-                  FluentIcons.text_bullet_list_square_clock_20_regular),
-              title: Text(
-                "Riwayat Pesanan",
-                style: GoogleFonts.poppins(
-                  textStyle: const TextStyle(
-                    fontSize: 17,
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                fixedSize: Size(MediaQuery.of(context).size.width - 25, 45),
+              ),
+              child: ListTile(
+                dense: true,
+                horizontalTitleGap: 10,
+                leading: const Icon(
+                    FluentIcons.text_bullet_list_square_clock_20_regular),
+                title: Text(
+                  "Riwayat Pesanan",
+                  style: GoogleFonts.poppins(
+                    textStyle: const TextStyle(
+                      fontSize: 17,
+                    ),
                   ),
                 ),
+                trailing: const Icon(FluentIcons.arrow_right_48_regular),
               ),
-              trailing: const Icon(FluentIcons.arrow_right_48_regular),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton.filled(
-                onPressed: () {},
-                icon: const Icon(FluentIcons.person_support_24_regular),
-                style: IconButton.styleFrom(
-                  backgroundColor: const Color.fromRGBO(196, 13, 15, 1),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton.filled(
+                  onPressed: () {},
+                  icon: const Icon(FluentIcons.person_support_24_regular),
+                  style: IconButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(196, 13, 15, 1),
+                  ),
                 ),
-              ),
-              IconButton.filled(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-                icon: const Icon(FluentIcons.arrow_exit_20_regular),
-                style: IconButton.styleFrom(
-                  backgroundColor: const Color.fromRGBO(196, 13, 15, 1),
+                IconButton.filled(
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                  },
+                  icon: const Icon(FluentIcons.arrow_exit_20_regular),
+                  style: IconButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(196, 13, 15, 1),
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
